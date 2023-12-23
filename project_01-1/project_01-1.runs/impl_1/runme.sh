b69e3688e31ce60c958b6488d7a4528bcc8d6d49
maydaym3 <jamestschirhart@gmail.com>
2023-12-22 17:31:44 -0800

    project1-1

--- project_01-1/project_01-1.runs/impl_1/runme.sh
+++ project_01-1/project_01-1.runs/impl_1/runme.sh
@@ -41,7 +41,7 @@ EAStep()
 }
 
 # pre-commands:
-/bin/touch .init_design.begin.rst
+/bin/touch .write_bitstream.begin.rst
 EAStep vivado -log count_wrap.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source count_wrap.tcl -notrace
 
 
